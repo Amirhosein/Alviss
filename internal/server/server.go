571b42c4e33f672de7fb7a12f6df04aabf44f762
Amir Iravanimanesh <airavanimanesh@gmail.com>
2021-12-17 21:36:31 +0330

    feat: db initialization added

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,16 +2,37 @@ package server
 
 import (
 	"fmt"
+	"net/http"
 
+	db "github.com/amirhosein/alviss/internal/DB"
+	"github.com/amirhosein/alviss/internal/handler"
 	"github.com/labstack/echo/v4"
 )
 
+var (
+	// ServerPort is the port on which the server is running
+	ServerPort string
+)
+
 func RunServer(port string) {
+	ServerPort = port
 	fmt.Println("Server is running on port " + port)
+	db.InitializeStore()
 
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(200, "Hello, World!")
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"message": "Welcome to Alviss, your mythical URL shortener",
+		})
 	})
+
+	e.POST("/shorten", func(c echo.Context) error {
+		return handler.CreateShortUrl(c, ServerPort)
+	})
+
+	e.GET("/:shortUrl", func(c echo.Context) error {
+		return handler.HandleShortUrlRedirect(c)
+	})
+
 	e.Logger.Fatal(e.Start(":" + port))
 }
