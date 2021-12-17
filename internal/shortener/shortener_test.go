f4fa02b957f818de8a94dbde042b443a09fcd460
Amir Iravanimanesh <airavanimanesh@gmail.com>
2021-12-17 21:37:11 +0330

    test: shortener_test added

---
+++ internal/shortener/shortener_test.go
@@ -0,0 +1,24 @@
+package shortener
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const UserId = "e0dba740-fc4b-4977-872c-d360239e6b1a"
+
+func TestShortLinkGenerator(t *testing.T) {
+	initialLink_1 := "https://www.guru3d.com/news-story/spotted-ryzen-threadripper-pro-3995wx-processor-with-8-channel-ddr4,2.html"
+	shortLink_1 := GenerateShortLink(initialLink_1, UserId)
+
+	initialLink_2 := "https://www.eddywm.com/lets-build-a-url-shortener-in-go-with-redis-part-2-storage-layer/"
+	shortLink_2 := GenerateShortLink(initialLink_2, UserId)
+
+	initialLink_3 := "https://spectrum.ieee.org/automaton/robotics/home-robots/hello-robots-stretch-mobile-manipulator"
+	shortLink_3 := GenerateShortLink(initialLink_3, UserId)
+
+	assert.Equal(t, shortLink_1, "jTa4L57P")
+	assert.Equal(t, shortLink_2, "d66yfx7N")
+	assert.Equal(t, shortLink_3, "dhZTayYQ")
+}
