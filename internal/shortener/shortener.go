58d130bc502ed1bf74127adf2df65ce552577864
Amir Iravanimanesh <airavanimanesh@gmail.com>
2021-12-17 21:36:58 +0330

    feat: shortener functions implemented

---
+++ internal/shortener/shortener.go
@@ -0,0 +1,33 @@
+package shortener
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"math/big"
+	"os"
+
+	"github.com/itchyny/base58-go"
+)
+
+func sha256Of(input string) []byte {
+	algorithm := sha256.New()
+	algorithm.Write([]byte(input))
+	return algorithm.Sum(nil)
+}
+
+func base58Encoded(bytes []byte) string {
+	encoding := base58.BitcoinEncoding
+	encoded, err := encoding.Encode(bytes)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return string(encoded)
+}
+
+func GenerateShortLink(initialLink string, userId string) string {
+	urlHashBytes := sha256Of(initialLink + userId)
+	generatedNumber := new(big.Int).SetBytes(urlHashBytes).Uint64()
+	finalString := base58Encoded([]byte(fmt.Sprintf("%d", generatedNumber)))
+	return finalString[:8]
+}
