373cf82918514f63b8b948db2fc7bb6d68a237bb
Amir Iravanimanesh <airavanimanesh@gmail.com>
2021-12-18 18:10:32 +0330

    feat: counting func added

--- internal/DB/db.go
+++ internal/DB/db.go
@@ -32,17 +32,24 @@ func InitializeStore() *DBService {
 	return dbService
 }
 
-func SaveUrlMapping(shortUrl string, originalUrl string, userId string, ExpireTime time.Duration) error {
+func SaveUrlMapping(shortUrl string, urlMapping UrlMapping, ExpireTime time.Duration) error {
 	if dbService.redisClient == nil {
 		return fmt.Errorf("redis client is not initialized")
 	}
-	return dbService.redisClient.Set(shortUrl, originalUrl, ExpireTime).Err()
+	return dbService.redisClient.Set(shortUrl, urlMapping, ExpireTime).Err()
 }
 
 func RetrieveInitialUrl(shortUrl string) string {
 	result, err := dbService.redisClient.Get(shortUrl).Result()
 	if err != nil {
+		panic(err)
+	}
+	urlMapping := &UrlMapping{}
+	urlMapping.UnmarshalBinary([]byte(result))
+	if err != nil {
 		panic(fmt.Sprintf("Failed RetrieveInitialUrl url | Error: %v - shortUrl: %s\n", err, shortUrl))
 	}
-	return result
+	urlMapping.Count++
+	dbService.redisClient.Set(shortUrl, urlMapping, 0)
+	return urlMapping.Original_url
 }
