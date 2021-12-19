d27471f3088d0061c4fed729f7c7f8d3d22a43ab
Amir Iravanimanesh <airavanimanesh@gmail.com>
2021-12-19 16:46:23 +0330

    fix: fixed db_test

--- internal/DB/db_test.go
+++ internal/DB/db_test.go
@@ -2,7 +2,9 @@ package db
 
 import (
 	"testing"
+	"time"
 
+	"github.com/amirhosein/alviss/internal/shortener"
 	"github.com/amirhosein/alviss/internal/util"
 	"github.com/stretchr/testify/assert"
 )
@@ -18,16 +20,18 @@ func TestStoreInit(t *testing.T) {
 }
 
 func TestInsertionAndRetrieval(t *testing.T) {
-	initialLink := "https://www.guru3d.com/news-story/spotted-ryzen-threadripper-pro-3995wx-processor-with-8-channel-ddr4,2.html"
-	userUUId := "e0dba740-fc4b-4977-872c-d360239e6b1a"
-	shortURL := "Jsz4k57oAX"
+	urlMapping := new(UrlMapping)
+
+	urlMapping.Original_url = "https://www.guru3d.com/news-story/spotted-ryzen-threadripper-pro-3995wx-processor-with-8-channel-ddr4,2.html"
 	expireTime := util.GetExpireTime("2d")
+	urlMapping.ExpTime = time.Now().Add(expireTime)
+	shortURL := shortener.GenerateShortLink(urlMapping.Original_url)
 
 	// Persist data mapping
-	SaveUrlMapping(shortURL, initialLink, userUUId, expireTime)
+	SaveUrlMapping(shortURL, *urlMapping, expireTime)
 
 	// Retrieve initial URL
 	retrievedUrl := RetrieveInitialUrl(shortURL)
 
-	assert.Equal(t, initialLink, retrievedUrl)
+	assert.Equal(t, urlMapping.Original_url, retrievedUrl)
 }
