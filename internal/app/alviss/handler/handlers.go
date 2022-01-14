c5a549bd0bac71c0573b3952a6968f373eb712ba
Amir Iravanimanesh <airavanimanesh@gmail.com>
2022-01-14 16:48:34 +0330

    Revert "feat: added response package"
    This reverts commit 1956701258f50e5475fd4c73be048a70ba22be9d.

--- internal/app/alviss/handler/handlers.go
+++ internal/app/alviss/handler/handlers.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/amirhosein/alviss/internal/app/alviss/model"
 	"github.com/amirhosein/alviss/internal/app/alviss/request"
-	"github.com/amirhosein/alviss/internal/app/alviss/response"
 	"github.com/amirhosein/alviss/internal/app/alviss/util"
 
 	"github.com/labstack/echo/v4"
@@ -19,18 +18,24 @@ type Handler struct {
 }
 
 func (h Handler) Home(c echo.Context) error {
-	return response.Welcome(c)
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "Welcome to Alviss! Your mythical URL shortener",
+	})
 }
 
 func (h Handler) CreateShortURL(c echo.Context) error {
 	urlCreationRequest := new(request.URLCreationRequest)
 
 	if err := c.Bind(urlCreationRequest); err != nil {
-		return response.InvalidRequest(c)
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"message": "Invalid request body",
+		})
 	}
 
 	if err := urlCreationRequest.Validate(); err != nil {
-		return response.Error(c, err)
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"message": err.Error(),
+		})
 	}
 
 	urlMapping := model.URLMapping{
@@ -43,10 +48,15 @@ func (h Handler) CreateShortURL(c echo.Context) error {
 
 	err := h.URLRepo.Save(shortURL, urlMapping, util.GetExpireTime(urlCreationRequest.ExpDate))
 	if err != nil {
-		return response.Error(c, err)
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": err.Error(),
+		})
 	}
 
-	return response.SuccessfullyCreated(c, h.Port, shortURL)
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message":  "short url created successfully",
+		"ShortURL": "http://localhost:" + h.Port + "/" + shortURL,
+	})
 }
 
 func (h Handler) HandleShortURLRedirect(c echo.Context) error {
@@ -56,10 +66,14 @@ func (h Handler) HandleShortURLRedirect(c echo.Context) error {
 		log.Println(err)
 	}
 	if (model.URLMapping{}) == result {
-		return response.NotFound(c)
+		return c.JSON(http.StatusNotFound, map[string]interface{}{
+			"message": "Short url not found",
+		})
 	}
 	if !result.ExpTime.IsZero() && result.ExpTime.Before(time.Now()) {
-		return response.Expired(c)
+		return c.JSON(http.StatusNotFound, map[string]interface{}{
+			"message": "Short url expired",
+		})
 	}
 	result.Count++
 	err = h.URLRepo.Update(shortURL, result)
@@ -73,11 +87,20 @@ func (h Handler) HandleShortURLDetail(c echo.Context) error {
 	shortURL := c.Param("shortURL")
 	result, err := h.URLRepo.Get(shortURL)
 	if err != nil {
-		return response.Error(c, err)
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": err.Error(),
+		})
 	}
 	if (model.URLMapping{}) == result {
-		return response.NotFound(c)
+		return c.JSON(http.StatusNotFound, map[string]interface{}{
+			"message": "Short url not found",
+		})
 	} else {
-		return response.Detail(c, result, shortURL, h.Port)
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"OriginalURL": result.OriginalURL,
+			"ShortURL":    "http://localhost:" + h.Port + "/" + shortURL,
+			"UsedCount":   result.Count,
+			"ExpDate":     result.ExpTime,
+		})
 	}
 }
